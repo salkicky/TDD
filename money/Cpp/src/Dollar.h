92b31683eee20954027170c7b1e53246c8c6f9ac
salkicky <salkicky@gmail.com>
2012-05-25 22:45:31 +0900

    add src

---
+++ money/Cpp/src/Dollar.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Money.h"
+
+class Dollar : private Money {
+public:
+	Dollar() {amount = 0;}
+
+	Dollar(int amount) {
+		this->amount = amount;
+	}
+
+	Dollar times(int multiplier) {
+		Dollar ap(this->amount * multiplier);
+		return ap;
+	}
+
+	bool equals(Dollar &object) {
+		Money *money = &object;
+		return amount == money->amount;
+	}
+
+	bool operator==(Dollar &other) {
+		return this->equals(other);
+	}
+};
