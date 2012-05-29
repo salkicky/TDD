3c43e64bc2c601ab440083c9f21c15c43c51e1a3
salkicky <salkicky@gmail.com>
2012-05-29 12:49:48 +0900

    remove refarence of Dollar class from Test

--- money/Cpp/src/Test.cpp
+++ money/Cpp/src/Test.cpp
@@ -10,15 +10,15 @@ TEST(TestMultiplication, Test) {
 }
 
 TEST(TestEquqlity, Test) { 
-	ASSERT_TRUE( Dollar(5).equals(Dollar(5)) );
-	ASSERT_FALSE( Dollar(5).equals(Dollar(6)) );
+	ASSERT_TRUE( Money::dollar(5).equals(Money::dollar(5)) );
+	ASSERT_FALSE( Money::dollar(5).equals(Money::dollar(6)) );
 	ASSERT_TRUE( Franc(5).equals(Franc(5)) );
 	ASSERT_FALSE( Franc(5).equals(Franc(6)) );
-	ASSERT_FALSE( Franc(5).equals(Dollar(5)) );
+	ASSERT_FALSE( Franc(5).equals(Money::dollar(5)) );
 }
 
 TEST(TestCurrency, Test) {
-	ASSERT_STREQ("USD", Dollar(5).currency().c_str());
+	ASSERT_STREQ("USD", Money::dollar(5).currency().c_str());
 	ASSERT_STREQ("FRN", Franc(5).currency().c_str());
 }
 
